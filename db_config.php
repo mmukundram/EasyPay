6649c42b677cfbaf78e6a1602117cb715c616935
mmukundram <mmukundram@gmail.com>
2017-12-19 17:24:58 +0530

    Database connection in server

---
+++ db_config.php
@@ -0,0 +1,19 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+/* 
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', ""); // db password (mention your db password here)
+define('DB_DATABASE', "easypay"); // database name
+define('DB_SERVER', "localhost:3306"); // db server
+*/
+
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "easypay";
+
+?>
---
+++ db_connect.php
@@ -0,0 +1,8 @@
+<?php
+require 'db_config.php';
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
